caa0c89f137a7c85f34a98b0fd7922dd6213a1a8
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2025-03-10 01:40:35 +0530

    Add Terraform configuration for creating an EC2 instance in AWS

---
+++ SimpleScripts/createEC2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  # Set your desired AWS region
+}
+
+
+resource "aws_instance" "this" {
+  ami                     = "ami-05c179eced2eb9b5b"
+  instance_type           = "t2.micro"
+  key_name                = "terraform-test-key"
+  subnet_id               = "subnet-0df92f64b59bcaf30"
+}
